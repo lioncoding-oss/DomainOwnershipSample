af6577990db0bbbb8d1196fe22ac379ede09306e
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2024-01-01 19:53:58 +0100

    added project files

---
+++ DomainOwnershipSample/Features/DomainVerifications/DependencyInjection.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace DomainOwnershipSample.Features.DomainVerifications;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBackgroundServices(this IServiceCollection services)
+    {
+        services.AddQuartz(q =>
+        {
+            var jobKey = new JobKey(nameof(ProcessVerificationJob));
+            q.AddJob<ProcessVerificationJob>(opts => opts.WithIdentity(jobKey));
+            
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity($"{nameof(ProcessVerificationJob)}-trigger")
+                /*.WithSimpleSchedule(schedule =>
+                    schedule.WithIntervalInMinutes(5).RepeatForever()
+                )*/
+                .WithCronSchedule("0 0/5 * * * ? *") // Every 5 minutes
+            );
+        });
+        
+        services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+        return services;
+    }
+}
